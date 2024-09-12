5869f0c14bc405379e5afed110622934c8a0b511
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:23:08 +0100

    Adding and committing channels.php

---
+++ ALXBackEnd/routes/channels.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+}, ['guards' => 'sanctum']);
