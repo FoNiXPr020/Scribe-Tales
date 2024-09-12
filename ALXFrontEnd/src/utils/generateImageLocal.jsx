5005cd45ce7525205641a3fa97f98073c032646f
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:54:22 +0100

    Add generateImageLocal utility

---
+++ ALXFrontEnd/src/utils/generateImageLocal.jsx
@@ -0,0 +1,19 @@
+import { Client } from "@gradio/client";
+
+const generateImageLocal = async (prompt) => {
+  const client = await Client.connect("prodia/sdxl-stable-diffusion-xl");
+  const result = await client.predict("/flip_text", {
+      prompt: "Generate an image based on my story: " + prompt,
+      negative_prompt: "3d, cartoon, anime, (deformed eyes, nose, ears, nose), bad anatomy, ugly",
+      model: "dreamshaperXL10_alpha2.safetensors [c8afe2ef]",
+      steps: 25,
+      sampler: "DPM++ 3M SDE Exponential",
+      cfg_scale: 7,
+      resolution: "1344x768",
+      seed: -1,
+  });
+
+  return result.data[0].url;
+};
+
+export default generateImageLocal;
