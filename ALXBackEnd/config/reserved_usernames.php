bdf3bec508bf674d0919bb97e70e6363b72d93cf
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:23:04 +0100

    Adding and committing reserved_usernames.php

---
+++ ALXBackEnd/config/reserved_usernames.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'login',
+    'register',
+    'forgot-password',
+    'reset-password',
+    'explore',
+    'profile',
+    'contact',
+    'my-stories',
+    'create-story'
+];
