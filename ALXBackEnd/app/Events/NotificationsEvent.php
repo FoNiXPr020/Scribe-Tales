bb95231ab75ad7329ecfe7cec0ab99e41035ff2c
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:22:59 +0100

    Adding and committing NotificationsEvent.php

---
+++ ALXBackEnd/app/Events/NotificationsEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationsEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    public $notification;
+    public function __construct( $notification )
+    {
+        //
+        $this->notification = $notification;
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'notification' => $this->notification
+        ];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('notifications_channel'),
+        ];
+    }
+}
