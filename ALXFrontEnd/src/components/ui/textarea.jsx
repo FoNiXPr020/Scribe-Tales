51d0c76a5c7d4be2d33a9ac9dd384a3355618c3b
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:23:42 +0100

    Add Textarea component

---
+++ ALXFrontEnd/src/components/ui/textarea.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Textarea = React.forwardRef(({ className, ...props }, ref) => {
+  return (
+    (<textarea
+      className={cn(
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-foreground focus:border-foreground disabled:cursor-not-allowed disabled:opacity-50  disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props} />)
+  );
+})
+Textarea.displayName = "Textarea"
+
+export { Textarea }
