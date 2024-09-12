aa32ddb3925a9e9ab606edfe8f99fc8654af873e
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-12 21:23:07 +0100

    Adding and committing api.php

---
+++ ALXBackEnd/routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\StoryController;
+use App\Http\Controllers\API\CommentController;
+use App\Http\Controllers\API\FollowerController;
+use App\Http\Controllers\API\LikeController;
+use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\API\NotificationController;
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/auth/google', [GoogleController::class, 'loginWithGoogle']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    route::post('/check/follow/{username}', [FollowerController::class, 'checkFollow']);
+
+    Route::post('/follow/{username}', [FollowerController::class, 'follow']);
+    Route::delete('/unfollow/{username}', [FollowerController::class, 'unfollow']);
+    Route::post('/like/{story}', [LikeController::class, 'like']);
+    Route::delete('/unlike/{story}', [LikeController::class, 'unlike']);
+});
+
+// Route for auth user profile ApiResource
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Authenticated routes | User profile and stories
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/user/stories', [StoryController::class, 'userStories']);
+    Route::post('/user/profile', [ProfileController::class, 'update']);
+    Route::post('/user/password', [ProfileController::class, 'updatePassword']);
+
+    // Authenticated routes | Stories
+    Route::apiResource('/stories', StoryController::class)->only(['store', 'update', 'destroy']);
+    Route::apiResource('/comments', CommentController::class)->only(['store', 'update', 'destroy']);
+
+    Route::get('/comments/{story_id}', [CommentController::class, 'index']);
+
+    // Authenticated routes | Notifications
+    Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::post('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsRead']);
+    Route::post('/notifications/{notification}', [NotificationController::class, 'update']);
+    Route::delete('/notifications/{notification}', [NotificationController::class, 'destroy']);
+});
+
+
+// Public routes
+Route::get('/users/{username}', [UserController::class, 'getUserByUsername']);
+Route::get('/users/stories/{username}', [StoryController::class, 'storiesByUser']);
+Route::get('/explore', [StoryController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/explore/{story_id}', [StoryController::class, 'show']);
+
+Route::get('/user/followers/{username}', [FollowerController::class, 'getUserFollowers']);
+Route::get('/user/likes/{username}', [LikeController::class, 'getUserLikes']);
+
+Route::get('/familiar', [StoryController::class, 'getFamiliarStoriesByType']);
+
+require __DIR__.'/auth.php';
