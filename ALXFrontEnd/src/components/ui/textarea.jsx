22de49c109f8e3c77e7a5403a2ebb9059e096ead
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-17 07:41:18 +0100

    Update UI Components rounded Style

--- ALXFrontEnd/src/components/ui/textarea.jsx
+++ ALXFrontEnd/src/components/ui/textarea.jsx
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     (<textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-foreground focus:border-foreground disabled:cursor-not-allowed disabled:opacity-50  disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[80px] w-full rounded-xl border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-foreground focus:border-foreground disabled:cursor-not-allowed disabled:opacity-50  disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       ref={ref}
