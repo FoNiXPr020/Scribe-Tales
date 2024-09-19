2fe70b4ef76b81b2509661c5bb23dea54c8a8480
FoNiXPr020 <rogueman2018@gmail.com>
2024-09-19 17:33:06 +0100

    Update Factory for stories testing images from local project

--- ALXBackEnd/database/factories/StoryFactory.php
+++ ALXBackEnd/database/factories/StoryFactory.php
@@ -13,6 +13,8 @@ use App\Models\Story;
 class StoryFactory extends Factory
 {
     protected $model = Story::class;
+    protected static $imageIndex = 0;
+
     /**
      * Define the model's default state.
      *
@@ -20,9 +22,10 @@ class StoryFactory extends Factory
      */
     public function definition(): array
     {
+        // List of genre types for the writer
         $genreList = [
             "Fantasy Writer",
-            "Historical Fiction",
+            "Historical Fiction Writer",
             "Sci-Fi Writer",
             "Romance Writer",
             "Dystopian Writer",
@@ -35,14 +38,36 @@ class StoryFactory extends Factory
             "Fiction Writer",
             "Horror Writer",
         ];
-       
+
+        // List of image URLs, assuming your images are accessible via a public URL
+        $imageList = [
+            url('storage/images/1.png'),
+            url('storage/images/2.png'),
+            url('storage/images/3.png'),
+            url('storage/images/4.png'),
+            url('storage/images/5.png'),
+            url('storage/images/6.png'),
+            url('storage/images/7.png'),
+            url('storage/images/8.png'),
+            url('storage/images/9.png'),
+            url('storage/images/10.png'),
+            url('storage/images/11.png'),
+            url('storage/images/12.png'),
+            url('storage/images/13.png'),
+            url('storage/images/14.png'),
+        ];
+
+        // Sequentially pick an image from the list
+        $image = $imageList[self::$imageIndex % count($imageList)];
+        self::$imageIndex++;
+
         return [
             'user_id' => User::factory(),
             'story_id' => Str::random(40),
-            'title' => fake()->sentence(4),
-            'image' => fake()->imageUrl(1334, 768, 'png'),
-            'content' => fake()->paragraph(),
-            'writer_type' => $genreList[array_rand($genreList)],
+            'title' => $this->faker->sentence(4),
+            'image' => $image, // Sequentially assign the image
+            'content' => $this->faker->paragraph(),
+            'writer_type' => $genreList[self::$imageIndex % count($genreList)], // Sequentially assign a genre
         ];
     }
 }
